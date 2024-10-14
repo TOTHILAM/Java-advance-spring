ddb8a1b5d20e79d5d3c0fc07f7909136529371a9
TOTHILAM <tolam0902@gmail.com>
2024-10-14 12:20:59 +0900

    create

--- src/main/java/com/vti/springframework/exception/ErrorHandler.java
+++ src/main/java/com/vti/springframework/exception/ErrorHandler.java
@@ -2,6 +2,8 @@ package com.vti.springframework.exception;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import org.hibernate.type.descriptor.java.ObjectJavaType;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -10,18 +12,24 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.LinkedHashMap;
 
 @ControllerAdvice
 public class ErrorHandler extends ResponseEntityExceptionHandler {
+    private String getMessage(String key, Object... args) {
+        var locale = LocaleContextHolder.getLocale();
+        return getMessageSource().getMessage(key, args, locale);
+    }
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException exception,
             HttpHeaders headers, HttpStatusCode status, WebRequest request
     ) {
-       var message = "Dữ liệu đầu vào không hợp lệ";
+       var message = getMessage("MethodArgumentNotValidException.message");
        var errors = new LinkedHashMap<String, String>();
        for (var error : exception.getFieldErrors()) {
            var key = error.getField();
@@ -36,7 +44,7 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handerConstraninViolation (
             ConstraintViolationException exception
     ) {
-        var message = "Dữ liệu đầu vào không hợp lệ";
+        var message = getMessage("MethodArgumentNotValidException.message");
         var errors = new LinkedHashMap<String, String>();
         for (var constrain : exception.getConstraintViolations()) {
             var key = constrain.getPropertyPath().toString();
